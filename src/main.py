02a4413187a492df7ed39a466edde9c5e327e16c
Trevor Elwell <me@trevorelwell.com>
2019-01-05 21:07:06 -0800

    getting tracker response

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
+import aiohttp
+import asyncio
+import bencode
 import os
 from torrent import Torrent
 
 # A torrent to test with for now
 TORRENT = os.path.abspath(os.path.join('examples', 'ubuntu.torrent'))
 
+
+async def main(some_torrent):
+    announce_url = some_torrent.get_announce_with_params()
+    async with aiohttp.ClientSession() as session:
+        async with session.get(announce_url) as response:
+            data = await response.read()
+            print(bencode.decode(data))
+
 if __name__ == '__main__':
     t = Torrent(TORRENT)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(t))
     print(t)
--- src/torrent.py
+++ src/torrent.py
@@ -1,12 +1,22 @@
 import bencode
+import random
 
 from hashlib import sha1
+from urllib.parse import urlencode
 
 
 class Torrent:
-    def __init__(self, torrent_path):
-        self.meta = {}
+    def __init__(self, torrent_path, port=6889, compact=True):
+        self.meta = None
+        self.peer_id = None
+        self.info_hash = None
+        self.uploaded = 0
+        self.downloaded = 0
+        self.left = None
+        self.port = port
+        self.compact = compact
         self._get_meta(torrent_path)
+        self._set_meta()
 
     def __str__(self):
         return f'Filename: {self.meta["info"]["name"]}\n \
@@ -15,10 +25,26 @@ class Torrent:
         Hash: {self.info_hash}'
 
     def _get_meta(self, torrent_path):
-        with open(torrent_path, 'rb') as f:
-            meta = f.read()
-            torrent = bencode.decode(meta)
-            self.meta = torrent
-            this_info = bencode.encode(self.meta['info'])
-            self.info_hash = sha1(this_info).digest()
+        self.meta = bencode.bread(torrent_path)
 
+    def _set_meta(self):
+        self.info_hash = sha1(bencode.encode(self.meta['info'])).digest()
+        self._get_peer_id()
+        self.left = self.meta['info']['length']
+
+    def _get_peer_id(self):
+        if self.peer_id is None:
+            self.peer_id = f'-PC0001-{"".join([str(random.randint(0, 9)) for _ in range(12)])}'
+        return self.peer_id
+
+    def get_announce_with_params(self):
+        params = {
+            'info_hash': self.info_hash,
+            'peer_id': self.peer_id,
+            'uploaded': self.uploaded,
+            'downloaded': self.downloaded,
+            'left': self.left,
+            'port': self.port,
+            'compact': '1' if self.compact else '0'
+        }
+        return f'{self.meta["announce"]}?{urlencode(params)}'
